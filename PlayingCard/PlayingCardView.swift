3ff232014576a78d9e05e45f4898d68de1f6fda9
slv177 <shambazov@gmail.com>
2018-01-28 12:27:10 +0300

    Начали draw (_rect)

---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by SS on 28/01/2018.
+//  Copyright © 2018 SS. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+
+}
---
+++ PlayingCard/PlayingCardsDeck.swift
@@ -0,0 +1,41 @@
+//
+//  PlayingCardsDeck.swift
+//  PlayingCard
+//
+//  Created by SS on 27/01/2018.
+//  Copyright © 2018 SS. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCardDeck {
+    private (set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw () -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+    }
+}
